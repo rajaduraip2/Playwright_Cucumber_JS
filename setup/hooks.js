cd75d4a79a6d73a1c2446e83f3d43224e953bb1c
Rajadurai <rajaduraip2@gmail.com>
2024-10-27 04:14:04 +0530

    Updates 27/10
    -Cucumber Html reporter
    -merged two jobs

--- setup/hooks.js
+++ setup/hooks.js
@@ -1,19 +1,50 @@
-const {BeforeAll,Before,After,AfterAll,BeforeStep,AfterStep}= require('@cucumber/cucumber');
+const {BeforeAll,Before,After,AfterAll,BeforeStep,AfterStep,setDefaultTimeout,attach}= require('@cucumber/cucumber');
 const{chromium}=require('@playwright/test');
+const path = require('path');
+const playwright= require('playwright')
 
+const options ={
+    headless:false,
+    slowMo:10
+}
+setDefaultTimeout(12*1000);
 
-
-
-BeforeAll(async()=>{
-    global.browser= await chromium.launch({ headless:false });
+BeforeAll({timeout:10*1000},async()=>{
+    global.browser= await playwright.chromium.launch(options);
 
     
 });
 
-Before(async()=>{
+Before({timeout:10*1000},async()=>{
+
     global.context= await global.browser.newContext();
     global.page=await global.context.newPage();
 });
-AfterAll(async()=>{
+
+After({timeout:10*1000},async()=>{
     await global.page.close();
+    await global.context.close();
+});
+
+// After({timeout:10*1000},async(scenario)=>{
+//     if(scenario.result.status=FAILED){
+
+//     const img= await global.page.screenshot({
+//         path:`test-results/screenshots/${scenario.pickle.name}.png`,
+//         fullPage: true
+//     });
+//  await this.attach(img,"image/png");
+
+      
+        
+//     }
+//     await global.page.close();
+//     await global.context.close();
+
+// })
+
+
+
+AfterAll({timeout:10*1000},async()=>{
+    await global.browser.close();
 });
--- step-definitions/login-steps.js
+++ step-definitions/login-steps.js
@@ -1,15 +1,12 @@
 const {Given,When,Then}=require("@cucumber/cucumber");
-// const {chromium,Browser, page,expect}=require("@playwright/test");
-// let browser=Browser;
-// let  global.page= global.page;
+ const {expect}=require("@playwright/test");
+
 
 
 
 Given('User navigates to the application', async function () {
-    // browser= await chromium.launch({ headless:false });
-    //  global.page=await browser.new global.page();
     await  global.page.goto("https://tutorialsninja.com/demo/");
-   // await expect(await  global.page.title()).tobe('Your Store');
+    await expect(global.page.title()).resolves.toBe('Your Store');
  
   });
 
@@ -23,9 +20,9 @@ Given('User click on the login link', async function () {
       
 
 Given('User enter the username as {string}', async function (username) {
-   // await expect(await  global.page.title()).tobe('Account Login');
-   const title= await  global.page.title();
-   console.log(title);
+     await expect(global.page.title()).resolves.toBe('Account Login');
+//    const title= await  global.page.title();
+//    console.log(title);
 await  global.page.locator("#input-email").fill(username);
          
          });
@@ -49,10 +46,7 @@ When('User click on the login button', async function () {
 
 
 Then('Login should be success', async function () {
-   const title= await  global.page.title()
-   console.log(title );
-    // global.page.close();
-         
+   await expect(global.page.title()).resolves.toBe('My Account');
          });
 
 
@@ -60,6 +54,5 @@ When('Login should fail', async function () {
     await expect(await  global.page.locator("//*[contains(@class,'alert-dismissible')]")).toBeVisible();
     const alertText=await  global.page.locator("//*[contains(@class,'alert-dismissible')]").textContent();
     console.log(alertText );
-    //  global.page.close();
          
          });
